adeb8b026050dd5f5a4cf6fea33f97f12b11893d
Jordie <xd@jrdie.nl>
2022-02-18 19:56:15 +0100

    Disable @SpringBootTest temporarily

--- parlio-tweede-kamer-graphql-api/src/test/kotlin/nl/parlio/api/tweedekamer/ParlioStembureauGraphqlApiApplicationTests.kt
+++ parlio-tweede-kamer-graphql-api/src/test/kotlin/nl/parlio/api/tweedekamer/ParlioStembureauGraphqlApiApplicationTests.kt
@@ -1,9 +1,8 @@
 package nl.parlio.api.tweedekamer
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+//@SpringBootTest
 class ParlioStembureauGraphqlApiApplicationTests {
 
     @Test
